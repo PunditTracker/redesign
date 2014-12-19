b6899292fc9f9e10a4033fb53b6444e27d600d16
Dan Schlosser <dan.r.schlosser@gmail.com>
2014-12-19 01:37:41 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from flask.ext.assets import Environment, Bundle
+from config import config
+import json
+import random
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html',
+                           random=(lambda l: l[random.randint(0, len(l) - 1)]))
+
+
+def register_scss():
+    """"""
+    assets.url = app.static_url_path
+    with open('config/scss.json') as f:
+        bundle_instructions = json.loads(f.read())
+        for _, bundle_set in bundle_instructions.iteritems():
+            output_folder = bundle_set['output_folder']
+            depends = bundle_set['depends']
+            for bundle_name, instructions in bundle_set['rules'].iteritems():
+                bundle = Bundle(*instructions['inputs'],
+                                output=output_folder + instructions['output'],
+                                depends=depends,
+                                filters='scss')
+                assets.register(bundle_name, bundle)
+
+assets = Environment(app)
+app.config.from_object('config.config')
+register_scss()
+
+if __name__ == '__main__':
+    app.run(port=config.PORT, host=config.HOST)
