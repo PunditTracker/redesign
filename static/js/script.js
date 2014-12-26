2d5b4dc5ea07b79ab4f2c518166590e95a83b143
Dan Schlosser <dan.r.schlosser@gmail.com>
2014-12-25 23:25:24 -0500

    add set

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,10 @@
 $(function() {
-    $('.hero').width($('.hero').width())
+    // $(window).scroll(function() {
+    //     var $hero = $('.hero'),
+    //         $bg = $('.hero > .background'),
+    //         difference = $(window).scrollTop() - $hero.offset().top,
+    //         percentComplete = Math.min(Math.max(0, difference)/$hero.height(), 1);
+    //     $bg.css({"margin-top": 200*percentComplete});
+    // });
 });
 
