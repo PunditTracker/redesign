b71045db1074c1150893232adcf38fbc333849ab
Dan Schlosser <dan.r.schlosser@gmail.com>
2014-12-28 18:17:10 -0500

    search box works

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,1 @@
-$(function() {
-    // $(window).scroll(function() {
-    //     var $hero = $('.hero'),
-    //         $bg = $('.hero > .background'),
-    //         difference = $(window).scrollTop() - $hero.offset().top,
-    //         percentComplete = Math.min(Math.max(0, difference)/$hero.height(), 1);
-    //     $bg.css({"margin-top": 200*percentComplete});
-    // });
-});
 
