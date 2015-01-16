8d44387396f027dbe9aeaa038bce783335990616
Dan Schlosser <dan.r.schlosser@gmail.com>
2015-01-16 14:19:29 -0500

    tags and madness

--- static/js/script.js
+++ static/js/script.js
@@ -22,4 +22,14 @@ $(function() {
 		console.log($filter.attr('class'));
 	});
 
+	var $pick = $('.pick'),
+		$madness = $('.march-madness'),
+		$window = $(window);
+
+	$window.scroll(function() {
+		if ($window.scrollTop() + $window.height() > $madness.offset().top + $madness.height()) {
+			$pick.addClass('fade-in-up')
+		}
+	})
+
 })
